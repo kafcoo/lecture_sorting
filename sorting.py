bab5fc7552b415e441aa1c39e675580ff454dba1
Tomáš Kaválek <tomas.kavalek88@icloud.com>
2024-04-17 09:51:37 +0200

    Add function selection_sort version: Teacher

--- sorting.py
+++ sorting.py
@@ -25,19 +25,36 @@ def read_data(file_name):
     return data
 
 
-def selection_sort(numbers_list, direction=True):  # direction = "True" (vzestupně), False (sestupně)
-    if direction:
-        for i in range(1, len(numbers_list)):
-            for j in range(len(numbers_list) - 1):
-                if numbers_list[j] > numbers_list[i]:
-                    numbers_list[j], numbers_list[i] = numbers_list[i], numbers_list[j]
-    else:
-        for i in range(1, len(numbers_list)):
-            for j in range(len(numbers_list) - 1):
-                if numbers_list[j] < numbers_list[i]:
-                    numbers_list[j], numbers_list[i] = numbers_list[i], numbers_list[j]
+def selection_sort(numbers_list, direction="ascending"):  # direction = "True" (vzestupně), False (sestupně)
+    for i in range(len(numbers_list)):
+        min_idx = i
+        for num_idx in range(i + 1, len(numbers_list)):
+            if direction == "ascending":
+                if numbers_list[min_idx] > numbers_list[num_idx]:
+                    min_idx = num_idx
+            elif direction == "descending":
+                if numbers_list[min_idx] < numbers_list[num_idx]:
+                    min_idx = num_idx
+        numbers_list[i], numbers_list[min_idx] = numbers_list[min_idx], numbers_list[i]
 
     return numbers_list
+
+    # noinspection PyUnreachableCode
+    """
+        if direction:
+            for i in range(1, len(numbers_list)):
+                for j in range(len(numbers_list) - 1):
+                    if numbers_list[j] > numbers_list[i]:
+                        numbers_list[j], numbers_list[i] = numbers_list[i], numbers_list[j]
+        else:
+            for i in range(1, len(numbers_list)):
+                for j in range(len(numbers_list) - 1):
+                    if numbers_list[j] < numbers_list[i]:
+                        numbers_list[j], numbers_list[i] = numbers_list[i], numbers_list[j]
+    
+        return numbers_list
+    
+    """
 
 
 def bubble_sort(numbers_list):
